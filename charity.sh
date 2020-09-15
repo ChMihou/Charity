fc6a14f7d5d43474571f4987a827bd489abf938b
Admin <rambo@mobplus.net>
2020-09-15 17:54:57 +0800

    项目运行脚本

--- charity.sh
+++ charity.sh
@@ -12,9 +12,20 @@ kill -9 $pidlist
 echo "KILL $pidlist:"
 fi
 }
+#检查执行语句失败  ==eq 执行成功  !=-ne 执行失败
+function check_fail(){
+        if [ $? -ne 0 ]; then
+                echo "ERROR: $1"
+                exit 1
+        fi
+}
+
 #执行方法
 stop
+#执行检验
+check_fail "重启项目失败"
 #启动jar包
 cd /home/
-nohup java -jar charity-0.0.1-SNAPSHOT.jar  > countyMedia.out 2>&1&
+
+nohup java -jar charity-0.0.1-SNAPSHOT.jar  > charity.out 2>&1&
 ~
